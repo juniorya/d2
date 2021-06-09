fb67625f8fa4a316eb2becf6519b35f9f52396aa
Mikhail Golyakov <junioryayaya@gmail.com>
2021-06-09 12:47:07 +0300

    add counter

--- d2_counter/d2_counter.ino
+++ d2_counter/d2_counter.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+#define TACH_PIN 2    // пин тахометра (желательна внешняя подтяжка 10к к VCC)
 
+#include <Tachometer.h>
+Tachometer tacho;
+
+#include <GyverUART.h>
+
+#include <GyverOS.h>
+GyverOS<3> OS;
+
+int long counter = 0;
+
+void setup() {
+  pinMode(LED_BUILTIN,OUTPUT);
+  uart.begin();
+
+  // put your setup code here, to run once:
+  pinMode(TACH_PIN, INPUT_PULLUP);
+
+  // настраиваем прерывание
+  attachInterrupt(0, isr, FALLING);
+
+  OS.attach(0, f1, 10);
+  OS.attach(1, f2, 500);
 }
 
+void isr() {
+  tacho.tick();   // сообщаем библиотеке об этом
+  counter++;
+}
+  //Serial.println(tacho.getRPM());			// RPM
+	//Serial.println(tacho.getHz());		// Hz
 void loop() {
   // put your main code here, to run repeatedly:
+  OS.tick();
+  //delay(OS.getLeft());
+}
 
+void f1() {
+  // выводит свой период в порт
+  uart.print(tacho.getUs());	// us
+  uart.print(',');
+  uart.println(counter);
+}
+
+void f2() {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
