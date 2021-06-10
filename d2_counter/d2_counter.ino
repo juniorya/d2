418e598dc030125a4b12fa28206cb3bfb1d02922
Mikhail Golyakov <junioryayaya@gmail.com>
2021-06-10 10:40:37 +0300

    add libs

--- d2_counter/d2_counter.ino
+++ d2_counter/d2_counter.ino
@@ -8,24 +8,25 @@ Tachometer tacho;
 #include <GyverOS.h>
 GyverOS<3> OS;
 
-#include <SPI.h>
-#include <SD.h>
-File myFile;
+#include <EEPROM.h>
 
 int long counter = 0;
 
 void setup() {
   pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(10,OUTPUT);
+  digitalWrite(10, 0);
   uart.begin();
 
   // put your setup code here, to run once:
   pinMode(TACH_PIN, INPUT_PULLUP);
 
   // настраиваем прерывание
-  attachInterrupt(0, isr, FALLING);
+  attachInterrupt(0, isr, CHANGE);
 
   OS.attach(0, f1, 10);
   OS.attach(1, f2, 500);
+  OS.attach(2, f3, 1000);
 }
 
 void isr() {
@@ -50,3 +51,27 @@ void f1() {
 void f2() {
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
+
+void f3() {
+  digitalWrite(10,!digitalRead(10));
+}
+
+/*
+0. Чтение EEPROM, если пусто, то ожидание меню.
+1. Если доступен UART открыть меню
+2. Меню - время измерения, задержка запуска счетчика, генератор 1Гц-10кГц, колличество импульсов, ip адрес, сохранить/отменить сохранение в eeprom
+3. Запись на SDCARD если файл есть создать новый +1 
+4. poll в Eth W5100 + log на SDCARD
+5. Формат?
+*/
+struct{
+  int timeC = 0;
+  int delayS = 0;
+  int freq = 0;
+  int freqC = 0;
+  byte ippart1 = 192;
+  byte ippart2 = 192;
+  byte ippart3 = 192;
+  byte ippart4 = 192;
+  //ip[] = { 192, 168, 1, 177 };  
+} settings;
